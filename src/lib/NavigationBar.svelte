212526b4c93643cf080ff7663feaa288e3388bd1
Nathan Jang <nathanjang12@gmail.com>
2024-12-13 18:35:50 +0800

    init

---
+++ src/lib/NavigationBar.svelte
@@ -0,0 +1,28 @@
+<nav>
+	<a href="/">
+		<h1>Colorful Ambience</h1>
+	</a>
+
+
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/app">App</a></li>
+	</ul>
+</nav>
+
+<style>
+nav {
+	display: flex;
+	justify-content: space-between;
+	padding: 1rem;
+}
+
+ul {
+	display: flex;
+}
+
+ul li {
+	padding: 0 0 0 1rem;
+	list-style: none;
+}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import "../app.css";
+	import NavigationBar from "$lib/NavigationBar.svelte";
+
+	let { children } = $props();
+</script>
+
+<NavigationBar/>
+
+{@render children()}
