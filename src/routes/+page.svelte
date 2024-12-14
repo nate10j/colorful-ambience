3ce479fd06849875c99e0d90c705095c9010acd0
Nathan Jang <nathanjang12@gmail.com>
2024-12-14 16:26:50 +0800

    create landing page and style it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,76 @@
-<a href="/app">Play sounds</a>
+<section class="hero">
+	<div class="container">
+		<div class="content">
+			<h1>
+				Play noises
+				<br/>
+				that help you <span class="focus">focus</span>
+			</h1>
+			<p class="description">
+				Listen to ambient brown noise, white noise and pink noise<br/>
+				and block out distracting sounds to relax, sleep or increase focus
+			</p>
+			<div class="cta">
+				<a href="/app">Play noise</a>
+				<p>no signup required!</p>
+				</div>
+			</div>
+		</div>
+</section>
 
 <style>
+.hero {
+	margin-bottom: 50px;
+}
+.hero .container {
+	background-size: contain;
+	background-position: center bottom;
+	max-width: 1170;
+	height: 550px;
+	padding: 0 5rem;
+}
+.hero .content {
+	width: 70%;
+}
 
+.hero h1 {
+	font-weight: 800;
+	font-size: 3.5rem;
+	margin: 3rem 0;
+	color: #462525;
+}
+
+.hero .description {
+	width: 70%;
+	font-size: 1.2rem;
+	font-weight: 350;
+	margin-bottom: 3.2rem;
+	color: #804444;
+}
+
+.focus {
+	font-style: italic;
+	font-weight: 900;
+}
+
+.cta a {
+	padding: 0.8rem 3rem;
+	border-radius: 0.4rem;
+	background-color: #F6A296;
+	color: #552D2D;
+	font-weight: 600;
+	font-size: 1.2rem;
+	text-decoration: none;
+}
+
+.cta p {
+	margin-top: 1rem;
+	font-size: 0.96rem;
+	font-weight: 450;
+	color: #703B3B;
+}
+
+.cta a:hover {
+	background-color: #F3928B;
+}
 </style>
