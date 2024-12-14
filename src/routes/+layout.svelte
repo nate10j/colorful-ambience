1c33c3e42782cde5bff25b4206805aa9720330de
Nathan Jang <nathanjang12@gmail.com>
2024-12-14 12:54:09 +0800

    add inter font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
-	import "../app.css";
+	// font
+	import "../styles/inter.css"
+
+	import "../styles/app.css";
+
+	// components
 	import NavigationBar from "$lib/NavigationBar.svelte";
 
 	let { children } = $props();
