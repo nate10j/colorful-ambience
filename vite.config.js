1b46df5744eb0d0cf4ab57f749af8695d3beb054
Nathan Jang <nathanjang12@gmail.com>
2024-12-17 20:05:48 +0800

    correctly set up wasm

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
+import wasmPack from 'vite-plugin-wasm-pack';
+
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), wasmPack(["./noise-generator"])],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
