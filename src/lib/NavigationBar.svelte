d79d2bd3c0497210bf03e369936b707b643d3e9c
Nathan Jang <nathanjang12@gmail.com>
2024-12-18 17:32:47 +0800

    css tweaks responsiveness

--- src/lib/NavigationBar.svelte
+++ src/lib/NavigationBar.svelte
@@ -15,9 +15,35 @@ nav {
 	border-bottom: 2px solid #FFD8B8;
 }
 
+h1 {
+	font-size: 2rem;
+	font-weight: 900;
+	margin-right: 1rem;
+}
+
+ul li {
+	padding: 0 1rem;
+	list-style: none;
+	font-size: 1.2rem;
+	font-weight: 600;
+}
+
 @media(max-width: 600px) {
 	nav {
 		padding: 1rem 2rem;
+	}
+
+	h1 {
+		font-size: 1.6rem;
+	}
+}
+@media(max-width: 400px) {
+	h1 {
+		font-size: 1.4rem;
+	}
+
+	ul li {
+		padding: 0 0.6rem;
 	}
 }
 
@@ -26,21 +52,8 @@ a {
 	text-decoration: none;
 }
 
-h1 {
-	font-size: 2rem;
-	font-weight: 900;
-	margin-right: 1rem;
-}
-
 ul {
 	display: flex;
 	align-items: center;
-}
-
-ul li {
-	padding: 0 1rem;
-	list-style: none;
-	font-size: 1.2rem;
-	font-weight: 600;
 }
 </style>
