ae566c94926ab259857be1a9a25c32b692994a9e
Nathan Jang <nathanjang12@gmail.com>
2025-01-19 14:28:15 +0800

    fix base path issue

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,6 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
