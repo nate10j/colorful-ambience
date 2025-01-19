ce47b919a4767d6473ac5f36efc694e981aac9da
Nathan Jang <nathanjang12@gmail.com>
2025-01-19 14:14:32 +0800

    fix base path issue

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
+	base: "/colorful-ambience/",
 	plugins: [sveltekit()], 
 
 	test: {
